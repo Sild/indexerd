fc7fea2a670c418982c0bb0893c278b9bbcc19b0
Sild <sildtm@icloud.com>
2023-10-16 20:23:15 +0200

    fix proto integration

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 extern crate prost_build;
+use std::fs;
 
 fn main() {
+    fs::create_dir_all("src/proto").unwrap();
     prost_build::Config::new()
         .out_dir("src/proto")
         .compile_protos(&["proto/req.proto"], &["proto"])
